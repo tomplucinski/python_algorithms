a5c5e34b5dd1bc32fbc973cf72cc5545a3588cd4
Tom Plucinski <tapluci@gmail.com>
2023-04-03 17:20:14 -0700

    Implement generateDocument function

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 from isPalindrome import isPalindrome
 from shiftCharacters import shiftCharacters
 from runLengthEncoding import runLengthEncoding
+from charDocumentGenerator import generateDocument
 
 print(isPalindrome('asbbsa'))
 print(shiftCharacters('abc', 3))
 print(runLengthEncoding('AAAAAAAAAAAAABBCCCCDD'))
+print(generateDocument("Bste!hetsi ogEAxpelrt x ", "AlgoExpert is the Best!"))
